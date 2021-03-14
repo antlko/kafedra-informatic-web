c72d2dc21507ec5bf395dc0287dd5dfc72ce80ea
Anatol Kozhukhar <anatol.kozhukhar@paysolut.com>
2021-03-14 16:32:12 +0200

    react fix

--- src/scripts/editor.js
+++ src/scripts/editor.js
@@ -9,7 +9,6 @@ import Raw from "@editorjs/raw";
 import Header from "@editorjs/header";
 import Quote from "@editorjs/quote";
 import Marker from "@editorjs/marker";
-import CheckList from "@editorjs/checklist";
 import Delimiter from "@editorjs/delimiter";
 import InlineCode from "@editorjs/inline-code";
 import SimpleImage from "@editorjs/simple-image";
@@ -33,7 +32,6 @@ export const EDITOR_JS_TOOLS = {
     raw: Raw,
     header: Header,
     quote: Quote,
-    checklist: CheckList,
     delimiter: Delimiter,
     inlineCode: InlineCode,
     simpleImage: SimpleImage
